1e801e747d1aa9e6ecc7e4e269c2db3524b8c3a9
Phichayapa Phemphoon <phichayapa.phemphoon@gmail.com>
2026-01-20 01:04:27 +0700

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,18 +1,16 @@
 from torchvision import transforms
 import torch
-import numpy as np
 
 def smart_crop(image):
     """
     crop กลางภาพ + bias ลงล่าง
-    ได้หน้า + คอ + ไหล่แน่นอน
+    ได้หน้า + คอ + ไหล่
     """
     w, h = image.size
-
     crop_size = int(min(w, h) * 0.85)
 
     cx = w // 2
-    cy = int(h * 0.45)  # bias ลงล่าง 🔥
+    cy = int(h * 0.45)
 
     left = max(cx - crop_size // 2, 0)
     top = max(cy - crop_size // 2, 0)
@@ -20,3 +18,19 @@ def smart_crop(image):
     bottom = min(top + crop_size, h)
 
     return image.crop((left, top, right, bottom))
+
+
+def preprocess_image(image):
+    image = smart_crop(image)
+
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # ✅ ตามที่เพื่อนบอก + ตรงตอน train
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+    x = transform(image).unsqueeze(0)
+    return x

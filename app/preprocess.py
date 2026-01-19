b8a9ca0ad68f15e70083fc9bc06b80e25e5fef96
Phichayapa Phemphoon <phichayapa.phemphoon@gmail.com>
2026-01-20 00:49:39 +0700

    Update preprocess.py

--- app/preprocess.py
+++ app/preprocess.py
@@ -1,17 +1,15 @@
-import torch
-from torchvision import transforms
+def preprocess_image(image):
+    image = smart_crop(image)
 
-def preprocess_image(pil_image):
     transform = transforms.Compose([
-        transforms.Resize((224, 224)),   # 🔥 ตรงกับตอน train
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],  # ImageNet
+            mean=[0.485, 0.456, 0.406],
             std=[0.229, 0.224, 0.225]
         )
     ])
 
-    x = transform(pil_image)
-    x = x.unsqueeze(0)  # (1, 3, 224, 224)
-
+    x = transform(image).unsqueeze(0)
     return x

0549582d49962ec02901d9fab35959b685aa0994
Phichayapa Phemphoon <phichayapa.phemphoon@gmail.com>
2026-01-20 00:28:56 +0700

    Update preprocess.py

--- app/preprocess.py
+++ app/preprocess.py
@@ -1,6 +1,17 @@
-import numpy as np
+import torch
+from torchvision import transforms
 
-def preprocess(pil_face):
-    img = pil_face.resize((224, 224))
-    img = np.array(img) / 255.0
-    return img
+def preprocess_image(pil_image):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),   # 🔥 ตรงกับตอน train
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],  # ImageNet
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+    x = transform(pil_image)
+    x = x.unsqueeze(0)  # (1, 3, 224, 224)
+
+    return x

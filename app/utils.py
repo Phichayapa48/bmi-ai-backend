80b8ff93175e5bf471b9bc9cdf121d63513e8b3c
Phichayapa Phemphoon <phichayapa.phemphoon@gmail.com>
2026-01-20 02:46:54 +0700

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,39 +1,10 @@
-import cv2
-import numpy as np
-import torch
-from PIL import Image
 from torchvision import transforms
-
-# =========================
-# Face detector
-# =========================
-face_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-)
-
-def detect_and_crop_face(pil_img):
-    img = np.array(pil_img.convert("RGB"))
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-    faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(80, 80)
-    )
-
-    if len(faces) == 0:
-        return None
-
-    x, y, w, h = faces[0]
-    face = img[y:y+h, x:x+w]
-    return Image.fromarray(face)
 
 # =========================
 # Preprocess (ตรงตอน train)
 # =========================
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),  # ✅ ตรง train
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
@@ -41,5 +12,5 @@ transform = transforms.Compose([
     )
 ])
 
-def preprocess_image(face_img):
-    return transform(face_img).unsqueeze(0)
+def preprocess_image(img):
+    return transform(img).unsqueeze(0)
